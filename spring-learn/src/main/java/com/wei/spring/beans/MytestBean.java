12be526a5439238089620f29461738483d13c2b1
Welmanhotheart <a274936580@163.com>
2021-04-03 10:47:01 +0800

    commit rest

---
+++ spring-learn/src/main/java/com/wei/spring/beans/MytestBean.java
@@ -0,0 +1,12 @@
+package com.wei.spring.beans;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MytestBean {
+	private String testStr = "testStr";
+
+}
---
+++ spring-learn/src/test/java/com/wei/spring/factory/test/BeanFactoryTest.java
@@ -0,0 +1,16 @@
+package com.wei.spring.factory.test;
+
+import com.wei.spring.beans.MytestBean;
+import org.junit.Test;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class BeanFactoryTest {
+
+	@Test
+	public void testSimpleLoad() {
+		XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+		MytestBean myTestBean = (MytestBean) xmlBeanFactory.getBean("myTestBean");
+		System.out.println(myTestBean);
+	}
+}
